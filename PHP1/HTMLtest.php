4ff14ea9daa87311c1fd94583cce073500b5af76
Ella Brant <ellapwiley@gmail.com>
2026-09-24 13:49:26 -0400

    HTML page with dynamic shopping list
    Create an HTML page with a PHP script to generate a shopping list based on inventory.

---
+++ PHP1/HTMLtest.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+  <body>
+    <p>Ella's shopping list</p>
+  </body>
+</html>
+
+<?php
+  $inventory = [
+    "milk" => 1,
+    "eggs" => 2,
+    "cereal" => 3,
+    "bread" => 4,
+    "mayo" => 5,
+    "ramen" => 6,
+    "juice" => 7,
+    "bagels" => 8,
+    "shampoo" => 9,
+    "conditioner" => 10
+  ];
+
+  $my_shopping_list = [];
+
+  foreach ($inventory as $item => $number) {
+    if ($number < 5) {
+        $my_shopping_list[] = $item; 
+    }
+}
+?>
+<br>
+<?php foreach ($my_shopping_list as $item): ?>
+      <li><?php echo $item; ?></li>
+<?php endforeach; 
+
+?>
